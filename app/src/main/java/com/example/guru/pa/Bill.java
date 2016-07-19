fc30a2f99424b326e4d8a37a5d75ba11319af6b1
Zak_eeee <zakeeee@163.com>
2016-07-19 22:30:55 +0800

    zakicommit

--- app/src/main/java/com/example/guru/pa/Bill.java
+++ app/src/main/java/com/example/guru/pa/Bill.java
@@ -5,68 +5,94 @@ import android.content.pm.ApplicationInfo;
 import android.graphics.Color;
 import android.graphics.Rect;
 import android.graphics.drawable.ColorDrawable;
+import android.support.annotation.NonNull;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /*import com.baoyz.swipemenulistview.SwipeMenu;
 import com.baoyz.swipemenulistview.SwipeMenuCreator;
 import com.baoyz.swipemenulistview.SwipeMenuItem;
 import com.baoyz.swipemenulistview.SwipeMenuListView;*/
 
+import com.baoyz.swipemenulistview.SwipeMenu;
+import com.baoyz.swipemenulistview.SwipeMenuCreator;
+import com.baoyz.swipemenulistview.SwipeMenuItem;
+import com.baoyz.swipemenulistview.SwipeMenuListView;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
 
 public class Bill extends AppCompatActivity {
 
-    private List<ApplicationInfo> mAppList;
-//    private SwipeMenuListView mListView;
+    /* 必备的三个量：一个List,一个Adapter,一个ListView */
+    private ArrayList<String> strs;
+    private ArrayAdapter<String> arrayAdapter;
+    private SwipeMenuListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bill);
-/*
-        mAppList = getPackageManager().getInstalledApplications(0);
+
+
+        /* 此处是示例 */
+        strs = new ArrayList<String>();
+        strs.add(0,"000");
+        strs.add(1,"111");
+        strs.add(2,"222");
+
+        /* 实例化ArrayAdapter */
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, strs);
+
+        /* 实例化SwipeMenuListView */
         mListView = (SwipeMenuListView) findViewById(R.id.bill_list);
 
+        /* 设置mListView的View最小高度 */
+        mListView.setMinimumHeight(180);
+
+        /* 实例化SwipeMenuCreator */
         SwipeMenuCreator creator = new SwipeMenuCreator() {
             @Override
             public void create(SwipeMenu menu) {
-                // create "open" item
+                /* create "open" item */
                 SwipeMenuItem openItem = new SwipeMenuItem(getApplicationContext());
-                // set item background
                 openItem.setBackground(new ColorDrawable(Color.rgb(0xC9, 0xC9, 0xCE)));
-                // set item width
-                openItem.setWidth(90);
-                // set item title
+                openItem.setWidth(180);
                 openItem.setTitle("Open");
-                // set item title fontsize
                 openItem.setTitleSize(18);
-                // set item title font color
-                openItem.setTitleColor(Color.WHITE);
-                // add to menu
+                openItem.setTitleColor(Color.rgb(0x00, 0x00, 0x00));
+                // 添加到SwipeMenu
                 menu.addMenuItem(openItem);
-                // create "delete" item
+
+                /* create "delete" item */
                 SwipeMenuItem deleteItem = new SwipeMenuItem(getApplicationContext());
-                // set item background
                 deleteItem.setBackground(new ColorDrawable(Color.rgb(0xF9, 0x3F, 0x25)));
-                // set item width
-                deleteItem.setWidth(90);
-                // set a icon
-                deleteItem.setIcon(R.drawable.ic_plus);
-                // add to menu
+                deleteItem.setWidth(180);
+                deleteItem.setTitle("X"); /* 未来会换成icon */
+                deleteItem.setTitleSize(18);
+                deleteItem.setTitleColor(Color.WHITE);
+                // 添加到SwipeMenu
                 menu.addMenuItem(deleteItem);
             }
         };
 
+        /* 给mListView设置Adapter,MenuCreator,设置滑动方向 */
+        mListView.setAdapter(arrayAdapter);
         mListView.setMenuCreator(creator);
         mListView.setSwipeDirection(SwipeMenuListView.DIRECTION_LEFT);
 
+        /* 给mListView添加按钮点击监听 */
         mListView.setOnMenuItemClickListener(new SwipeMenuListView.OnMenuItemClickListener() {
             @Override
             public boolean onMenuItemClick(int position, SwipeMenu menu, int index) {
@@ -75,6 +101,8 @@ public class Bill extends AppCompatActivity {
                         // open
                         break;
                     case 1:
+                        strs.remove(position);
+                        arrayAdapter.notifyDataSetChanged();
                         // delete
                         break;
                 }
@@ -82,7 +110,7 @@ public class Bill extends AppCompatActivity {
                 return false;
             }
         });
-
+/*
 */
     }
 
