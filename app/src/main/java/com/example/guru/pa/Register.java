753a9a5ef67ba63926031a120143c579f8ebefea
Zak_eeee <zakeeee@163.com>
2016-07-21 17:15:54 +0800

    Merge branch 'master' of https://github.com/stdiohero/SummerCode
    # Conflicts:
    #	app/src/main/java/com/example/guru/pa/LogIn.java
    #	app/src/main/java/com/example/guru/pa/MainActivity.java
    #	app/src/main/java/com/example/guru/pa/Register.java

--- app/src/main/java/com/example/guru/pa/Register.java
+++ app/src/main/java/com/example/guru/pa/Register.java
@@ -1,8 +1,11 @@
 package com.example.guru.pa;
 
+import android.os.StrictMode;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -12,6 +15,17 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.widget.Toast;
 import android.database.Cursor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Register extends AppCompatActivity {
     private EditText reg_username_edit;
@@ -25,11 +39,16 @@ public class Register extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+
         reg_username_edit = (EditText)findViewById(R.id.reg_username_edit);
         reg_password1_edit = (EditText)findViewById(R.id.reg_password1_edit);
         reg_password2_edit = (EditText)findViewById(R.id.reg_password2_edit);
         button = (Button)findViewById(R.id.button);
-        db = SQLiteDatabase.openOrCreateDatabase(this.getFilesDir().toString()+"/test.dbs", null);
+
+        /*db = SQLiteDatabase.openOrCreateDatabase(this.getFilesDir().toString()+"/test.dbs", null);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -72,12 +91,37 @@ public class Register extends AppCompatActivity {
                     }
                 }
             }
+        });*/
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = reg_username_edit.getText().toString();
+                String password = reg_password1_edit.getText().toString();
+                String password1 = reg_password2_edit.getText().toString();
+
+                if (name.equals("") || password.equals("")||password1.equals("")){
+                    Toast.makeText(Register.this,"请将帐号密码填完整", Toast.LENGTH_SHORT).show();
+                }else if(!password.equals(password1)){
+                    Toast.makeText(Register.this,"两次输入密码不一致", Toast.LENGTH_SHORT).show();
+                }else {
+                    StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+                    StrictMode.setThreadPolicy(policy);
+
+                    String req = "username="+name+"&password="+password;
+                    Toast.makeText(Register.this, HttpPostRequestor.doPost("http://192.168.1.101:8000/regist/", req), Toast.LENGTH_SHORT).show();
+                }
+
+            }
         });
+
     }
     // 添加用户
     public Boolean addUser(String name, String password) {
         String str = "insert into tb_user values(?,?) ";
-        LogIn usr = new LogIn();usr.db = db;
+        LogIn usr = new LogIn();
+        usr.db = db;
         try{
             db.execSQL(str, new String[]{name, password});
             return true;
@@ -88,7 +132,9 @@ public class Register extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        db.close();
+        if(db!=null) {
+            db.close();
+        }
     }
 
     @Override
@@ -96,11 +142,12 @@ public class Register extends AppCompatActivity {
         int id = item.getItemId();
         switch (id) {
             case android.R.id.home:
-                finish();
+                this.finish();
                 return true;
             default:
                 break;
         }
         return super.onOptionsItemSelected(item);
     }
+
 }
